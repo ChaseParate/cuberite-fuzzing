639de157aa3a72f23dabea172e1eae0bf9b928d3
Max Petersen <maxvonpetersen@gmail.com>
2026-03-04 20:00:02 -0700

    added pytest, fixed a bug in the varint implementation

--- fuzzing/boofuzz/varint.py
+++ fuzzing/boofuzz/varint.py
@@ -1,7 +1,10 @@
+import struct
 
 SEGMENT_BITS = 0x7f
 CONTINUE_BIT = 0x80
 
+# Note: this doesn't sign numbers (Python numbers are infinite)
+# you have to do that yourself
 def read_varnum(varnum: bytes, max_len: int) -> int:
     value: int = 0
     position: int = 0
@@ -15,14 +18,17 @@ def read_varnum(varnum: bytes, max_len: int) -> int:
         position += 7
         if position >= max_len:
             raise RuntimeError(f"VarInt is too big: maximum length {max_len} bits")
+        i += 1
     
     return value
 
 def read_varint(varint: bytes) -> int:
-    return read_varnum(varint, 32)
+    byte_value = struct.pack(">L", read_varnum(varint, 32))
+    return struct.unpack(">l", byte_value)[0]
 
 def read_varlong(varlong: bytes) -> int:
-    return read_varnum(varlong, 64)
+    byte_value = struct.pack(">Q", read_varnum(varlong, 64))
+    return struct.unpack(">q", byte_value)[0]
 
 def write_varnum(varnum: int, bit_size: int) -> bytes:
     value = bytearray()
---
+++ fuzzing/boofuzz/varint_test.py
@@ -0,0 +1,46 @@
+import pytest
+from varint import *
+
+VARINT_EXAMPLES = [
+    (0,           b"\x00"),
+    (1,           b"\x01"),
+    (2,           b"\x02"),
+    (127,         b"\x7f"),
+    (128,         b"\x80\x01"),
+    (255,         b"\xff\x01"),
+    (25565,       b"\xdd\xc7\x01"),
+    (2097151,     b"\xff\xff\x7f"),
+    (2147483647,  b"\xff\xff\xff\xff\x07"),
+    (-1,          b"\xff\xff\xff\xff\x0f"),
+    (-2147483648, b"\x80\x80\x80\x80\x08")
+]
+
+VARLONG_EXAMPLES = [
+    (0,                    b"\x00"),
+    (1,                    b"\x01"),
+    (2,                    b"\x02"),
+    (127,                  b"\x7f"),
+    (128,                  b"\x80\x01"),
+    (255,                  b"\xff\x01"),
+    (2147483647,           b"\xff\xff\xff\xff\x07"),
+    (9223372036854775807,  b"\xff\xff\xff\xff\xff\xff\xff\xff\x7f"),
+    (-1,                   b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\x01"),
+    (-2147483648,          b"\x80\x80\x80\x80\xf8\xff\xff\xff\xff\x01"),
+    (-9223372036854775808, b"\x80\x80\x80\x80\x80\x80\x80\x80\x80\x01")
+]
+
+@pytest.mark.parametrize("number,varint", VARINT_EXAMPLES)
+def test_read_varint(number: int, varint: bytes):
+    assert read_varint(varint) == number
+
+@pytest.mark.parametrize("number,varint", VARINT_EXAMPLES)
+def test_write_varint(number: int, varint: bytes):
+    assert write_varint(number) == varint
+
+@pytest.mark.parametrize("number,varlong", VARLONG_EXAMPLES)
+def test_read_varlong(number: int, varlong: bytes):
+    assert read_varlong(varlong) == number
+
+@pytest.mark.parametrize("number,varlong", VARLONG_EXAMPLES)
+def test_write_varlong(number: int, varlong: bytes):
+    assert write_varlong(number) == varlong
