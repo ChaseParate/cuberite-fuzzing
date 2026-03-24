fb55914720194326580feefe3252e4c4ea5d54c0
Max Petersen <maxvonpetersen@gmail.com>
2026-03-24 12:10:00 -0600

    format with ruff

--- fuzzing/models/__tests__/test_vectors.py
+++ fuzzing/models/__tests__/test_vectors.py
@@ -1,20 +1,29 @@
 import pytest
 
-from fuzzing.models.vectors import Position, PositionBlock, LpVec3, LpVec3Block
+from fuzzing.models.vectors import LpVec3, LpVec3Block, Position, PositionBlock
 
 POS_EXAMPLES = [
-    (Position(0, 0, 0),                  b'\x00\x00\x00\x00\x00\x00\x00\x00'),
-    (Position(-1, -1, -1),               b'\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'),
-    (Position(255, 255, 255),            int('0' * 18 + '1' * 8 + '0' * 18 + '1' * 8 + '0' * 4 + '1' * 8, 2).to_bytes(8)),
-    (Position(18357644, 831, -20882616), int('0100011000000111011000110010110000010101101101001000001100111111', 2).to_bytes(8)),
+    (Position(0, 0, 0), b"\x00\x00\x00\x00\x00\x00\x00\x00"),
+    (Position(-1, -1, -1), b"\xff\xff\xff\xff\xff\xff\xff\xff"),
+    (
+        Position(255, 255, 255),
+        int("0" * 18 + "1" * 8 + "0" * 18 + "1" * 8 + "0" * 4 + "1" * 8, 2).to_bytes(8),
+    ),
+    (
+        Position(18357644, 831, -20882616),
+        int(
+            "0100011000000111011000110010110000010101101101001000001100111111", 2
+        ).to_bytes(8),
+    ),
 ]
 
 VEC3_EXAMPLES = [
-    (LpVec3(0.0, 0.0, 0.0),         b'\x00'),
-    (LpVec3(1.0, 0.0, -1.0),        b'\xF1\xFF\x00\x00\xFF\xFF'),
-    (LpVec3(10.0, 0.2, -5.0),       b'\xF6\xFF\x40\x01\x05\x1F\x02'),
-    (LpVec3(123457.0, 15.071, 0.0), b'\xF5\xFF\x7F\xFF\x00\x07\x90\xF1\x01')
+    (LpVec3(0.0, 0.0, 0.0), b"\x00"),
+    (LpVec3(1.0, 0.0, -1.0), b"\xf1\xff\x00\x00\xff\xff"),
+    (LpVec3(10.0, 0.2, -5.0), b"\xf6\xff\x40\x01\x05\x1f\x02"),
+    (LpVec3(123457.0, 15.071, 0.0), b"\xf5\xff\x7f\xff\x00\x07\x90\xf1\x01"),
 ]
+
 
 @pytest.mark.parametrize(("position", "packed"), POS_EXAMPLES)
 def test_readwrite_position(position: Position, packed: bytes):
@@ -25,9 +34,11 @@ def test_readwrite_position(position: Position, packed: bytes):
     assert len(rest) == 0
     assert decoded == position
 
+
 @pytest.mark.parametrize(("vec3", "packed"), VEC3_EXAMPLES)
 def test_write_vec3(vec3: LpVec3, packed: bytes):
     assert vec3.write() == packed
+
 
 @pytest.mark.parametrize(("vec3", "packed"), VEC3_EXAMPLES)
 def test_read_vec3(vec3: LpVec3, packed: bytes):
@@ -37,6 +48,7 @@ def test_read_vec3(vec3: LpVec3, packed: bytes):
     assert abs(vec.x - vec3.x) < 0.001
     assert abs(vec.y - vec3.y) < 0.001
     assert abs(vec.z - vec3.z) < 0.001
+
 
 @pytest.mark.parametrize(("vec3", "packed"), VEC3_EXAMPLES)
 def test_vec3_block(vec3: LpVec3, packed: bytes):
--- fuzzing/models/vectors.py
+++ fuzzing/models/vectors.py
@@ -1,14 +1,19 @@
-import boofuzz, math
-from bitstring import BitStream, BitArray
+import math
 from dataclasses import dataclass
-from typing import override, Self
+from typing import Self, override
+
+import boofuzz
+from bitstring import BitArray, BitStream
+
 from fuzzing.models.varint import read_varint, write_varint
+
 
 @dataclass(frozen=True, slots=True)
 class Position:
     """
     Position literal: can be read from binary contents
     """
+
     x: int
     y: int
     z: int
@@ -17,24 +22,29 @@ class Position:
     def read(cls, data: bytes) -> tuple[Self, bytes]:
         assert len(data) >= 8
         s = BitStream(data)
-        x = s.read('int:26')
-        z = s.read('int:26')
-        y = s.read('int:12')
+        x = s.read("int:26")
+        z = s.read("int:26")
+        y = s.read("int:12")
         return (cls(x, y, z), data[8:])
 
+
 _MAX_QUANTIZED_VALUE = 32766.0
+
 
 def _unpack(value: int) -> float:
     return min(value & 32767, _MAX_QUANTIZED_VALUE) * 2.0 / _MAX_QUANTIZED_VALUE - 1.0
 
+
 def _pack(value: float) -> int:
     return round((value * 0.5 + 0.5) * _MAX_QUANTIZED_VALUE)
+
 
 @dataclass(frozen=True, slots=True)
 class LpVec3:
     """
     LpVec3 literal: can be read from binary contents
     """
+
     x: float
     y: float
     z: float
@@ -48,11 +58,11 @@ class LpVec3:
         length = 6
         vec3_bytes = bytes([data[2], data[3], data[4], data[5], data[1], data[0]])
         s = BitStream(vec3_bytes)
-        packed_z = s.read('uint:15')
-        packed_y = s.read('uint:15')
-        packed_x = s.read('uint:15')
-        continuation = s.read('bool')
-        scale = s.read('uint:2')
+        packed_z = s.read("uint:15")
+        packed_y = s.read("uint:15")
+        packed_x = s.read("uint:15")
+        continuation = s.read("bool")
+        scale = s.read("uint:2")
         if continuation:
             scale_msb = read_varint(data[6:])
             scale |= scale_msb.value << 2
@@ -61,11 +71,11 @@ class LpVec3:
         y = _unpack(packed_y) * scale
         z = _unpack(packed_z) * scale
         return (cls(x, y, z), data[length:])
-    
+
     def write(self) -> bytes:
         max_coord = max(abs(self.x), abs(self.y), abs(self.z))
         if max_coord < 3.051944088384301e-5:
-            return b'\x00'
+            return b"\x00"
         else:
             scale = int(math.ceil(max_coord))
             continuation = (scale & 3) != scale
@@ -81,7 +91,10 @@ class LpVec3:
             if continuation:
                 s.append(write_varint(scale >> 2))
             final = s.tobytes()
-            return bytes([final[5], final[4], final[0], final[1], final[2], final[3], *final[6:]])
+            return bytes(
+                [final[5], final[4], final[0], final[1], final[2], final[3], *final[6:]]
+            )
+
 
 class PositionBlock(boofuzz.FuzzableBlock):
     """Position primitive
@@ -92,37 +105,44 @@ class PositionBlock(boofuzz.FuzzableBlock):
 
     @override
     def __init__(
-            self,
-            name: str | None = None,
-            default: Position = Position(0, 0, 0),
-            fuzzable: bool = True,
-            *args,
-            **kwargs
+        self,
+        name: str | None = None,
+        default: Position = Position(0, 0, 0),
+        fuzzable: bool = True,
+        *args,
+        **kwargs,
     ):
         super().__init__(
             name=name,
             children=(
-                boofuzz.BitField("x", default.x, 26, endian=boofuzz.BIG_ENDIAN, signed=True),
-                boofuzz.BitField("z", default.z, 26, endian=boofuzz.BIG_ENDIAN, signed=True),
-                boofuzz.BitField("y", default.y, 12, endian=boofuzz.BIG_ENDIAN, signed=True)
+                boofuzz.BitField(
+                    "x", default.x, 26, endian=boofuzz.BIG_ENDIAN, signed=True
+                ),
+                boofuzz.BitField(
+                    "z", default.z, 26, endian=boofuzz.BIG_ENDIAN, signed=True
+                ),
+                boofuzz.BitField(
+                    "y", default.y, 12, endian=boofuzz.BIG_ENDIAN, signed=True
+                ),
             ),
             fuzzable=fuzzable,
             *args,
-            **kwargs
+            **kwargs,
         )
-    
+
     @override
     def encode(self, value, mutation_context) -> bytes:
         data: bytes = self.get_child_data(mutation_context=mutation_context)
         s = BitStream(data)
         out = BitArray()
-        s.read('bits:6')
-        out.append(s.read('bits:26'))
-        s.read('bits:6')
-        out.append(s.read('bits:26'))
-        s.read('bits:4')
-        out.append(s.read('bits:12'))
+        s.read("bits:6")
+        out.append(s.read("bits:26"))
+        s.read("bits:6")
+        out.append(s.read("bits:26"))
+        s.read("bits:4")
+        out.append(s.read("bits:12"))
         return out.tobytes()
+
 
 class LpVec3Block(boofuzz.FuzzableBlock):
     """LpVec3 Primitive
@@ -138,7 +158,7 @@ class LpVec3Block(boofuzz.FuzzableBlock):
         default: LpVec3 = LpVec3(0, 0, 0),
         fuzzable: bool = True,
         *args,
-        **kwargs
+        **kwargs,
     ):
         max_coord = max(abs(default.x), abs(default.y), abs(default.z))
         scale = int(math.ceil(max_coord))
@@ -146,18 +166,18 @@ class LpVec3Block(boofuzz.FuzzableBlock):
         y = 0 if max_coord == 0 else _pack(default.y / scale)
         z = 0 if max_coord == 0 else _pack(default.z / scale)
         super().__init__(
-            name = name,
-            children = (
+            name=name,
+            children=(
                 boofuzz.BitField("x", x, 15, endian=boofuzz.BIG_ENDIAN),
                 boofuzz.BitField("y", y, 15, endian=boofuzz.BIG_ENDIAN),
                 boofuzz.BitField("z", z, 15, endian=boofuzz.BIG_ENDIAN),
-                boofuzz.BitField("scale", scale, 31, endian=boofuzz.BIG_ENDIAN)
+                boofuzz.BitField("scale", scale, 31, endian=boofuzz.BIG_ENDIAN),
             ),
-            fuzzable = fuzzable,
+            fuzzable=fuzzable,
             *args,
-            **kwargs
+            **kwargs,
         )
-    
+
     @override
     def encode(self, value, mutation_context) -> bytes:
         data: bytes = self.get_child_data(mutation_context=mutation_context)
@@ -172,4 +192,3 @@ class LpVec3Block(boofuzz.FuzzableBlock):
         scale = s.read("uint:31")
         vec = LpVec3(_unpack(x) * scale, _unpack(y) * scale, _unpack(z) * scale)
         return vec.write()
-
