0ba62ed4946cf98df2d293e019078801cf1e21cc
Max Petersen <maxvonpetersen@gmail.com>
2026-03-24 11:47:39 -0600

    add a LpVec3 data type

--- fuzzing/models/__tests__/test_vectors.py
+++ fuzzing/models/__tests__/test_vectors.py
@@ -1,12 +1,19 @@
 import pytest
 
-from fuzzing.models.vectors import Position, PositionBlock
+from fuzzing.models.vectors import Position, PositionBlock, LpVec3
 
 POS_EXAMPLES = [
-    (Position(0, 0, 0),       b'\x00\x00\x00\x00\x00\x00\x00\x00'),
-    (Position(-1, -1, -1),    b'\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'),
-    (Position(255, 255, 255), int('0' * 18 + '1' * 8 + '0' * 18 + '1' * 8 + '0' * 4 + '1' * 8, 2).to_bytes(8)),
+    (Position(0, 0, 0),                  b'\x00\x00\x00\x00\x00\x00\x00\x00'),
+    (Position(-1, -1, -1),               b'\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'),
+    (Position(255, 255, 255),            int('0' * 18 + '1' * 8 + '0' * 18 + '1' * 8 + '0' * 4 + '1' * 8, 2).to_bytes(8)),
     (Position(18357644, 831, -20882616), int('0100011000000111011000110010110000010101101101001000001100111111', 2).to_bytes(8)),
+]
+
+VEC3_EXAMPLES = [
+    (LpVec3(0.0, 0.0, 0.0),         b'\x00'),
+    (LpVec3(1.0, 0.0, -1.0),        b'\xF1\xFF\x00\x00\xFF\xFF'),
+    (LpVec3(10.0, 0.2, -5.0),       b'\xF6\xFF\x40\x01\x05\x1F\x02'),
+    (LpVec3(123457.0, 15.071, 0.0), b'\xF5\xFF\x7F\xFF\x00\x07\x90\xF1\x01')
 ]
 
 @pytest.mark.parametrize(("position", "packed"), POS_EXAMPLES)
@@ -17,3 +24,16 @@ def test_readwrite_position(position: Position, packed: bytes):
     decoded, rest = Position.read(encoded)
     assert len(rest) == 0
     assert decoded == position
+
+@pytest.mark.parametrize(("vec3", "packed"), VEC3_EXAMPLES)
+def test_write_vec3(vec3: LpVec3, packed: bytes):
+    assert vec3.write() == packed
+
+@pytest.mark.parametrize(("vec3", "packed"), VEC3_EXAMPLES)
+def test_read_vec3(vec3: LpVec3, packed: bytes):
+    vec, rest = LpVec3.read(packed)
+    print(vec)
+    assert len(rest) == 0
+    assert abs(vec.x - vec3.x) < 0.001
+    assert abs(vec.y - vec3.y) < 0.001
+    assert abs(vec.z - vec3.z) < 0.001
--- fuzzing/models/vectors.py
+++ fuzzing/models/vectors.py
@@ -1,7 +1,8 @@
-import boofuzz
+import boofuzz, math
 from bitstring import BitStream, BitArray
 from dataclasses import dataclass
 from typing import override, Self
+from fuzzing.models.varint import read_varint, write_varint
 
 @dataclass(frozen=True, slots=True)
 class Position:
@@ -20,6 +21,67 @@ class Position:
         z = s.read('int:26')
         y = s.read('int:12')
         return (cls(x, y, z), data[8:])
+
+_MAX_QUANTIZED_VALUE = 32766.0
+
+def _unpack(value: int) -> float:
+    return min(value & 32767, _MAX_QUANTIZED_VALUE) * 2.0 / _MAX_QUANTIZED_VALUE - 1.0
+
+def _pack(value: float) -> int:
+    return round((value * 0.5 + 0.5) * _MAX_QUANTIZED_VALUE)
+
+@dataclass(frozen=True, slots=True)
+class LpVec3:
+    """
+    LpVec3 literal: can be read from binary contents
+    """
+    x: float
+    y: float
+    z: float
+
+    @classmethod
+    def read(cls, data: bytes) -> tuple[Self, bytes]:
+        assert len(data) >= 1
+        if data[0] == 0:
+            return (cls(0.0, 0.0, 0.0), data[1:])
+        assert len(data) >= 6
+        length = 6
+        vec3_bytes = bytes([data[2], data[3], data[4], data[5], data[1], data[0]])
+        s = BitStream(vec3_bytes)
+        packed_z = s.read('uint:15')
+        packed_y = s.read('uint:15')
+        packed_x = s.read('uint:15')
+        continuation = s.read('bool')
+        scale = s.read('uint:2')
+        if continuation:
+            scale_msb = read_varint(data[6:])
+            scale |= scale_msb.value << 2
+            length += scale_msb.length
+        x = _unpack(packed_x) * scale
+        y = _unpack(packed_y) * scale
+        z = _unpack(packed_z) * scale
+        return (cls(x, y, z), data[length:])
+    
+    def write(self) -> bytes:
+        max_coord = max(abs(self.x), abs(self.y), abs(self.z))
+        if max_coord < 3.051944088384301e-5:
+            return b'\x00'
+        else:
+            scale = int(math.ceil(max_coord))
+            continuation = (scale & 3) != scale
+            packed_x = _pack(self.x / scale)
+            packed_y = _pack(self.y / scale)
+            packed_z = _pack(self.z / scale)
+            s = BitArray()
+            s.append(f"uint:15={packed_z & 0x7FFF}")
+            s.append(f"uint:15={packed_y & 0x7FFF}")
+            s.append(f"uint:15={packed_x & 0x7FFF}")
+            s.append(f"bool={continuation}")
+            s.append(f"uint:2={scale & 3}")
+            if continuation:
+                s.append(write_varint(scale >> 2))
+            final = s.tobytes()
+            return bytes([final[5], final[4], final[0], final[1], final[2], final[3], *final[6:]])
 
 class PositionBlock(boofuzz.FuzzableBlock):
     """Position primitive
