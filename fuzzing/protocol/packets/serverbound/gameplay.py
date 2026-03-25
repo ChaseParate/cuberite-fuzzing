91817a45f533e53c35826c429914fb4836b42201
Max Petersen <maxvonpetersen@gmail.com>
2026-03-25 14:43:31 -0600

    move gameplay packets into 1 file, add a tab complete packet

---
+++ fuzzing/protocol/packets/serverbound/gameplay.py
@@ -0,0 +1,46 @@
+from boofuzz import Block, Byte, Fuzzable, String
+
+from fuzzing.models.varint_blocks import VarIntSized
+from fuzzing.models.vectors import Position, PositionBlock
+from fuzzing.protocol.packets.serverbound import create_compressed_packet
+
+
+def chat_packet(compression_threshold: int = 0, max_len: int | None = None) -> Fuzzable:
+    return create_compressed_packet(
+        "Chat",
+        0x2,
+        VarIntSized(
+            "Length",
+            children=[String("Chat Message", "Hello, World!", max_len=max_len)],
+        ),
+        compression_threshold,
+    )
+
+
+def tab_complete_packet(
+    compression_threshold: int = 0,
+    is_command: bool = False,
+    looking_at: Position | PositionBlock | None = None,
+) -> Fuzzable:
+    if looking_at is None:
+        position_segment = [Byte("Has Position", 0, fuzzable=False)]
+    elif isinstance(looking_at, Position):
+        position_segment = [
+            Byte("Has Position", 1, fuzzable=False),
+            PositionBlock("Looking At", looking_at, fuzzable=False),
+        ]
+    else:
+        position_segment = [Byte("Has Position", 1, fuzzable=False), looking_at]
+    return create_compressed_packet(
+        "Tab Complete",
+        0x1,
+        Block(
+            "Contents",
+            children=[
+                VarIntSized("Length", children=[String("Text")]),
+                Byte("Assume Command", 1 if is_command else 0, fuzzable=False),
+                *position_segment,
+            ],
+        ),
+        compression_threshold,
+    )
