0b152c8d374f172878d1be15cbebf47550b22338
Max Petersen <maxvonpetersen@gmail.com>
2026-03-25 17:18:34 -0600

    add plugin message packets

--- fuzzing/protocol/packets/serverbound/gameplay.py
+++ fuzzing/protocol/packets/serverbound/gameplay.py
@@ -1,4 +1,4 @@
-from boofuzz import Block, Byte, Fuzzable, String
+from boofuzz import Block, Byte, Bytes, Fuzzable, String
 
 from fuzzing.models.varint_blocks import VarIntSized
 from fuzzing.models.vectors import Position, PositionBlock
@@ -37,9 +37,32 @@ def tab_complete_packet(
         Block(
             "Contents",
             children=[
-                VarIntSized("Length", children=[String("Text")]),
+                VarIntSized("Length", children=[String("Text", "/he")]),
                 Byte("Assume Command", 1 if is_command else 0, fuzzable=False),
                 *position_segment,
+            ],
+        ),
+        compression_threshold,
+    )
+
+
+def plugin_message_packet(
+    compression_threshold: int = 0,
+    max_length: int | None = None,
+    max_channel_length: int | None = None,
+) -> Fuzzable:
+    return create_compressed_packet(
+        "Plugin Message",
+        0x9,
+        Block(
+            "Contents",
+            children=[
+                VarIntSized(
+                    "Length", children=[String("Channel", max_len=max_channel_length)]
+                ),
+                Bytes("Data")
+                if max_length is None
+                else Bytes("Data", max_len=max_length),
             ],
         ),
         compression_threshold,
