888ee07d51b121cd9258979f40eafd0df4c430b6
Chase Harkcom <me@chasehark.com>
2026-03-26 23:43:41 -0600

    Fix max receive size

--- fuzzing/fuzzer.py
+++ fuzzing/fuzzer.py
@@ -18,6 +18,7 @@ def fuzz(port: int, address: str):
         target=Target(
             connection=TCPSocketConnection(address, port),
             monitors=[MinecraftServer(["make", "run-cuberite"], address, port, state)],
+            max_recv_bytes=2**20,
         ),
         receive_data_after_each_request=False,
     )
